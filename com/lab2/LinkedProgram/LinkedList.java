0890cb7f74781112719b3958b8b2195d7adba8f9
Salim Bakri <salimbakri@salims-mbp.gateway.sasktel.net>
2019-09-29 00:16:39 -0600

    implement addElement and getElement

--- com/lab2/LinkedProgram/LinkedList.java
+++ com/lab2/LinkedProgram/LinkedList.java
@@ -7,4 +7,27 @@ public class LinkedList {
         le = head = tail = null;
         _nElements = 0;
     }
+
+    public void addElement(ListElement le) {
+        this.le = new ListElement();
+        this.le.setData(le.getData());
+        if (head == null) {
+            this.head = this.tail = this.le;
+        } else {
+            this.tail.setNext(this.le);
+            this.tail = this.le;
+        }
+        _nElements++;
+    }
+
+    public ListElement getElement(int index) {
+        if (index > _nElements) {
+            return null;
+        }
+        le = head;
+        for (int i = 0; i < index; i++) {
+            le = le.getNext();
+        }
+        return le;
+    }
 }
