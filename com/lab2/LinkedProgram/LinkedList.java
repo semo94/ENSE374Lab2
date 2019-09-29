48592480ce574ab7cd56d5cec8f28ca68e2cae9a
Salim Bakri <salimbakri@salims-mbp.gateway.sasktel.net>
2019-09-29 00:15:38 -0600

    create LinkedList class

---
+++ com/lab2/LinkedProgram/LinkedList.java
@@ -0,0 +1,10 @@
+package com.lab2.LinkedProgram;
+public class LinkedList {
+	private ListElement head, tail, le;
+    private int _nElements;
+
+    public LinkedList() {
+        le = head = tail = null;
+        _nElements = 0;
+    }
+}
---
+++ com/lab2/LinkedProgram/ListElement.java
@@ -0,0 +1,28 @@
+package com.lab2.LinkedProgram;
+public class ListElement {
+    private int data;
+    private ListElement next;
+
+    // class constructor
+    public ListElement() {
+        this.setData(0);
+        this.setNext(null);
+    }
+
+    // define getters and setters
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public void setNext(ListElement next) {
+        this.next = next;
+    }
+
+    public ListElement getNext() {
+        return next;
+    }
+}
