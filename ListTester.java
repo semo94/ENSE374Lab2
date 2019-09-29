27e6f5707a17af91588dcac32e6441ee9567957e
Salim Bakri <salimbakri@salims-mbp.gateway.sasktel.net>
2019-09-29 01:00:49 -0600

    test element deletion

--- ListTester.java
+++ ListTester.java
@@ -2,6 +2,8 @@ import com.lab2.LinkedProgram.*;
 
 public class ListTester {
     public static void main(String[] args) {
+        // print splash screen
+        System.out.println("***************** WELCOME TO THE TEST RUNNER *****************");
         // create linkedlist object and a node object
         ListElement le = new ListElement();
         LinkedList ll = new LinkedList();
@@ -9,7 +11,17 @@ public class ListTester {
         le.setData(69);
         ll.addElement(le);
         // Retrieve the added node
-        System.out.println("Retrieving node 0 from list, expecting 66...");
+        System.out.println("Retrieving node 0 from list, expecting 69...");
         System.out.println(ll.getElement(0).getData());
+        // Delete the node
+        System.out.println("Deleting node 0 from list, expecting 69...");
+        System.out.println(ll.deleteElement(0).getData());
+        // check if the list has been emptied correctly
+        System.out.println("Retrieving node from an empty list, expecting null...");
+        if (ll.getElement(0) == null) {
+            System.out.println("null");
+        } else {
+            System.out.println("not null");
+        }
     }
 }
